56a54b4ed47eb89086ce85fa9bbd464fff955536
Moinul Moin <contact@moinulmoin.com>
2023-07-02 23:20:01 +0600

    fix: eslint error and formate the code

--- src/root.tsx
+++ src/root.tsx
@@ -1,5 +1,9 @@
 import { component$ } from "@builder.io/qwik";
-import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from "@builder.io/qwik-city";
+import {
+  QwikCityProvider,
+  RouterOutlet,
+  ServiceWorkerRegister,
+} from "@builder.io/qwik-city";
 import { RouterHead } from "./components/layout/router-head";
 
 import "./global.css";
@@ -7,26 +11,26 @@ import { useTheme } from "./hooks/use-theme";
 import { cn } from "./shared/utils";
 
 export default component$(() => {
-	/**
-	 * The root of a QwikCity site always start with the <QwikCityProvider> component,
-	 * immediately followed by the document's <head> and <body>.
-	 *
-	 * Don't remove the `<head>` and `<body>` elements.
-	 */
+  /**
+   * The root of a QwikCity site always start with the <QwikCityProvider> component,
+   * immediately followed by the document's <head> and <body>.
+   *
+   * Don't remove the `<head>` and `<body>` elements.
+   */
 
-	const { darkMode } = useTheme();
+  const { darkMode } = useTheme();
 
-	return (
-		<QwikCityProvider>
-			<head>
-				<meta charSet="utf-8" />
-				<link rel="manifest" href="/manifest.json" />
-				<RouterHead />
-				<ServiceWorkerRegister />
-			</head>
-			<body lang="en" class={cn(darkMode.value ? "dark" : "light")}>
-				<RouterOutlet />
-			</body>
-		</QwikCityProvider>
-	);
+  return (
+    <QwikCityProvider>
+      <head>
+        <meta charSet="utf-8" />
+        <link rel="manifest" href="/manifest.json" />
+        <RouterHead />
+        <ServiceWorkerRegister />
+      </head>
+      <body lang="en" class={cn(darkMode.value ? "dark" : "light")}>
+        <RouterOutlet />
+      </body>
+    </QwikCityProvider>
+  );
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,23 +2,23 @@ import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
-	return (
-		<section class="min-h-[calc(100vh-108px)] py-40">
-			<div class="container">
-				<h1 class="text-4xl font-extrabold tracking-tight text-center lg:text-5xl animate-in zoom-in-0 duration-500">
-					Hello world, this is ChadQwik!
-				</h1>
-			</div>
-		</section>
-	);
+  return (
+    <section class="min-h-[calc(100vh-108px)] py-40">
+      <div class="container">
+        <h1 class="text-4xl font-extrabold tracking-tight text-center lg:text-5xl animate-in zoom-in-0 duration-500">
+          Hello world, this is ChadQwik!
+        </h1>
+      </div>
+    </section>
+  );
 });
 
 export const head: DocumentHead = {
-	title: "ChadQwik Template",
-	meta: [
-		{
-			name: "description",
-			content: "This is ChadQwik Template",
-		},
-	],
+  title: "ChadQwik Template",
+  meta: [
+    {
+      name: "description",
+      content: "This is ChadQwik Template",
+    },
+  ],
 };
